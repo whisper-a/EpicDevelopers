d19d58c5d6a2afd6379a250b441d1a2dab663dc8
rui.sun <rui.sun@joyworkshop.com>
2025-02-21 17:17:48 +0800

    first commit

---
+++ Source/EpicDevelopers/EpicDevelopers.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "EpicDevelopers.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, EpicDevelopers, "EpicDevelopers" );
+ 
